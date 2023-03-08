b3a371d4ac1eb8308a6abdef9fb98715288bb6a3
Julian Beldotti <julian.beldotti@gmail.com>
2023-03-08 13:51:35 -0500

    Feat: add overstocked items and total_inventory

--- lib/event.rb
+++ lib/event.rb
@@ -23,4 +23,35 @@ class Event
       food_truck.inventory.keys
     end.uniq
   end
+
+  def total_quantity(item)
+    food_trucks.flat_map do |food_truck|
+      food_truck.inventory[item]
+    end.sum
+  end
+
+  def overstocked_items
+    all_items.select do |item|
+      total_quantity(item) > 50 && food_trucks_that_sell(item).count > 1
+    end
+  end
+
+  def total_inventory
+    all_inventory = {}
+    food_trucks.flat_map do |food_truck|
+      food_truck.inventory.map do |ft_item, quantity|
+        if !all_inventory.has_key?(ft_item)
+          ft_trucks = Hash.new
+          ft_trucks[:quantity] = 0
+          ft_trucks[:food_trucks] = []
+          all_inventory[ft_item] = ft_trucks
+          ft_trucks[:quantity] = total_quantity(ft_item)
+          ft_trucks[:food_trucks] = all_items
+        else
+          all_inventory[ft_item] = total_quantity(ft_item)
+          all_inventory[ft_item] = all_items
+        end
+      end
+    end.uniq
+  end
 end
