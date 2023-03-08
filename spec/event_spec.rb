3fd9ae80e148755681bc93dc1f1bf35fbbd7ef58
Julian Beldotti <julian.beldotti@gmail.com>
2023-03-08 11:30:57 -0500

    Feat: init event class and pass test

---
+++ spec/event_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+RSpec.describe Event do
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      event = Event.new("South Pearl Street Farmers Market")
+      
+      expect(event.name).to eq("South Pearl Street Farmers Market")
+      expect(event.food_trucks).to eq([])
+    end
+  end
+end
