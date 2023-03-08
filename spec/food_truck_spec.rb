0c7af90b72f9282e92999edb939a5dd69ffb5775
Julian Beldotti <julian.beldotti@gmail.com>
2023-03-08 11:14:15 -0500

    Test: update food_truck spec name and tests

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+RSpec.describe FoodTruck do
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      food_truck = FoodTruck.new('Rocky Mountain Pies')
+
+      expect(food_truck.name).to eq('Rocky Mountain Pies')
+      expect(food_truck.inventory).to eq({})
+    end
+  end
+
+  describe 'check_stock' do
+    it 'can check if stock is empty' do
+      food_truck = FoodTruck.new('Rocky Mountain Pies')
+      item1 = Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' })
+
+      expect(food_truck.check_stock(item1)).to eq(0)
+    end
+
+    it 'can add stock' do
+      food_truck = FoodTruck.new('Rocky Mountain Pies')
+      item1 = Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' })
+      item2 = Item.new({ name: 'Apple Pie (Slice)', price: '$2.50' })
+      food_truck.stock(item1, 30)
+
+      expect(food_truck.inventory).to eq(item1 => 30)
+      expect(food_truck.check_stock(item1)).to eq(30)
+
+      food_truck.stock(item1, 25)
+
+      expect(food_truck.check_stock(item1)).to eq(55)
+
+      food_truck.stock(item2, 12)
+
+      expect(food_truck.inventory).to eq(item1 => 55, item2 => 12)
+    end
+  end
+end
